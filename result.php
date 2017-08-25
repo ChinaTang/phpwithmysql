a94252986d1b4506497fb41646c2a2d577f30f86
tangdi <tangdi0802@foxmail.com>
2017-08-25 15:29:36 +0800

    connect mysql

--- result.php
+++ result.php
@@ -19,4 +19,32 @@
         $searchterm = addslashes($searchterm);
     }
 
-    $db = new mysqli('localhost', "root", "1234", "books");
+    $db = new mysqli("", "root", "1234", "books");
+    if(mysqli_connect_errno()){
+        echo "Error : could not connect to database";
+        exit;
+    }else{
+        echo "mysql connect success";
+    }
+
+    $querySQL = "select * from books where ".$searchtype. " like '%".$searchterm."%' ";
+    echo "<br/>";var_dump($querySQL);
+    $reselt = $db->query($querySQL);
+    echo "<br/>";var_dump($reselt);
+    $num_result = $reselt->num_rows;
+
+    echo $num_result;
+
+    echo "Number of books found: ".$num_result;
+
+    for($i = 0; $i < $num_result; $i++){
+        $row = $reselt->fetch_assoc();
+        echo "Title".$i;
+        echo htmlspecialchars(stripslashes($row['title'])).'<br/>';
+        echo "Author:".stripslashes($row['author']).'<br/>';
+        echo "ISBN:".stripslashes($row['isbn']).'<br />';
+        echo "Prices:".stripslashes($row['price']).'<br />';
+    }
+
+    $reselt->free();
+    $db->close();
